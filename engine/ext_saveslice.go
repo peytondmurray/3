83c305a7a4139f837407bf1788751fc7de03aef0
pdmurray <peyton.murray@tuni.fi>
2019-03-04 10:15:32 +0200

    Removed Touko's code. Added function for saving domain wall configuration.

--- engine/ext_saveslice.go
+++ engine/ext_saveslice.go
@@ -7,10 +7,12 @@ import (
 
 var (
 	saveXYZIndex int // Stores the number of the next file to be written. Increments each time a file is written.
+	saveDWConfigIndex int
 )
 
 func init() {
 	DeclFunc("ext_saveVTK", saveVTK, "Save scalar slice of [][][]float64 values")
+	DeclFunc("ext_saveDWConfig", saveDWConfig, "Save domain wall configuration as csv.")
 }
 
 func check(e error) {
@@ -63,4 +65,43 @@ func saveVTK(s [][][]float64, name string) {
 
 	f.Sync()
 	saveXYZIndex++
+}
+
+func saveDWConfig(name string) {
+
+	n := MeshSize()
+	c := Mesh().CellSize()
+	dwpos := DWMonitor.dwpos
+
+	m := M.Buffer().HostCopy().Vectors()
+	dw := make([][6]float64, n[Z]*n[Y])		// (x, y, z, mx, my, mz) along the domain wall
+	k := 0
+	for i:=0; i<n[Z]; i++ {
+		for j:=0; j<n[Y]; j++ {
+			x := c[X]*float64(dwpos[i][j])
+			y := c[Y]*float64(j)
+			z := c[Z]*float64(i)
+			mx := float64(m[X][i][j][dwpos[i][j]])
+			my := float64(m[Y][i][j][dwpos[i][j]])
+			mz := float64(m[Z][i][j][dwpos[i][j]])
+			dw[k] = [6]float64{x, y, z, mx, my, mz}
+			k++
+		}
+	}
+
+	// Find current working dir
+	f, err := os.Create(fmt.Sprintf(OD()+name+"%06d.csv", saveDWConfigIndex)) //https://gobyexample.com/writing-files
+	check(err)
+
+	// Write timestamp
+	f.WriteString(fmt.Sprintf("#time = %e\n", Time))
+	f.WriteString(fmt.Sprintf("x,y,z,mx,my,mz\n"))
+
+	// Write values
+	for i := 0; i < len(dw); i++ {
+		f.WriteString(fmt.Sprintf("%E,%E,%E,%E,%E,%E\n", dw[i][0], dw[i][1], dw[i][2], dw[i][3], dw[i][4], dw[i][5]))
+	}
+
+	f.Sync()
+	saveDWConfigIndex++
 }

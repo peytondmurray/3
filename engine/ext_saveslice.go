991715c41b3f8670a8199f05b2b5aae4ac80822d
pdmurray <peynmurray@gmail.com>
2019-04-05 00:31:19 +0300

    Made saveDWConfig save the absolute domain wall location; made saveComovingDWConfig save the domain wall position in the simulation window.

--- engine/ext_saveslice.go
+++ engine/ext_saveslice.go
@@ -6,15 +6,17 @@ import (
 )
 
 var (
-	save2DIndex int
-	saveXYZIndex int // Stores the number of the next file to be written. Increments each time a file is written.
-	saveDWConfigIndex int
+	save2DIndex               int
+	saveXYZIndex              int // Stores the number of the next file to be written. Increments each time a file is written.
+	saveDWConfigIndex         int
+	saveComovingDWConfigIndex int
 )
 
 func init() {
 	DeclFunc("ext_save2D", save2D, "Save scalar slice of [][]float64 values")
 	DeclFunc("ext_saveVTK", saveVTK, "Save scalar slice of [][][]float64 values")
-	DeclFunc("ext_saveDWConfig", saveDWConfig, "Save domain wall configuration as csv.")
+	DeclFunc("ext_saveDWConfig", saveDWConfig, "Save absolute domain wall configuration as csv.")
+	DeclFunc("ext_saveComovingDWConfig", saveComovingDWConfig, "Save domain wall configuration in simulation window as csv.")
 }
 
 func check(e error) {
@@ -69,20 +71,21 @@ func saveVTK(s [][][]float64, name string) {
 	saveXYZIndex++
 }
 
+//
 func saveDWConfig(name string) {
 
 	n := MeshSize()
 	c := Mesh().CellSize()
-	dwpos := DWMonitor.dwpos
+	dwpos := DWMonitor.dwpos // Indices of DW in simulation window
 
 	m := M.Buffer().HostCopy().Vectors()
-	dw := make([][6]float64, n[Z]*n[Y])		// (x, y, z, mx, my, mz) along the domain wall
+	dw := make([][6]float64, n[Z]*n[Y]) // (x, y, z, mx, my, mz) along the domain wall
 	k := 0
-	for i:=0; i<n[Z]; i++ {
-		for j:=0; j<n[Y]; j++ {
-			x := c[X]*float64(dwpos[i][j])
-			y := c[Y]*float64(j)
-			z := c[Z]*float64(i)
+	for i := 0; i < n[Z]; i++ {
+		for j := 0; j < n[Y]; j++ {
+			x := c[X] * float64(dwpos[i][j]+DWMonitor.windowpos)
+			y := c[Y] * float64(j)
+			z := c[Z] * float64(i)
 			mx := float64(m[X][i][j][dwpos[i][j]])
 			my := float64(m[Y][i][j][dwpos[i][j]])
 			mz := float64(m[Z][i][j][dwpos[i][j]])
@@ -106,6 +109,45 @@ func saveDWConfig(name string) {
 
 	f.Sync()
 	saveDWConfigIndex++
+}
+
+func saveComovingDWConfig(name string) {
+
+	n := MeshSize()
+	c := Mesh().CellSize()
+	dwpos := DWMonitor.dwpos
+
+	m := M.Buffer().HostCopy().Vectors()
+	dw := make([][6]float64, n[Z]*n[Y]) // (x, y, z, mx, my, mz) along the domain wall
+	k := 0
+	for i := 0; i < n[Z]; i++ {
+		for j := 0; j < n[Y]; j++ {
+			x := c[X] * float64(dwpos[i][j])
+			y := c[Y] * float64(j)
+			z := c[Z] * float64(i)
+			mx := float64(m[X][i][j][dwpos[i][j]])
+			my := float64(m[Y][i][j][dwpos[i][j]])
+			mz := float64(m[Z][i][j][dwpos[i][j]])
+			dw[k] = [6]float64{x, y, z, mx, my, mz}
+			k++
+		}
+	}
+
+	// Find current working dir
+	f, err := os.Create(fmt.Sprintf(OD()+name+"%06d.csv", saveDWConfigIndex)) //https://gobyexample.com/writing-files
+	check(err)
+
+	// Write timestamp
+	f.WriteString(fmt.Sprintf("#time = %e\n", Time))
+	f.WriteString(fmt.Sprintf("x,y,z,mx,my,mz\n"))
+
+	// Write values
+	for i := 0; i < len(dw); i++ {
+		f.WriteString(fmt.Sprintf("%E,%E,%E,%E,%E,%E\n", dw[i][0], dw[i][1], dw[i][2], dw[i][3], dw[i][4], dw[i][5]))
+	}
+
+	f.Sync()
+	saveComovingDWConfigIndex++
 }
 
 func save2D(s [][]float64, name string) {

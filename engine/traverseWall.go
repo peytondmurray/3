5b72f1bb75617bc9154fb71afebe096b204bf4b2
pdmurray <peynmurray@gmail.com>
2019-04-16 21:01:43 +0300

    Fixed terrible unexpected behavior with append(). traceWall2D appears to work now.

--- engine/traverseWall.go
+++ engine/traverseWall.go
@@ -1,6 +1,7 @@
 package engine
 
 import (
+	// "fmt"
 	"math"
 )
 
@@ -17,48 +18,27 @@ func traceWall3D(mz [][][]float32) [][][2]int {
 // this function *might* trace that wall instead of the actual one you want.
 func traceWall2D(mz [][]float32) [][2]int {
 
-	zcs := allZCs(mz)
 	buffer := [2]int{}
-	zcs, buffer = pop(zcs)
 	path := make([][2]int, 1)
+
+	zcs := allZCs(mz)
+	zcs, buffer = pop(zcs)
 	path[0] = buffer
 
 	for len(zcs) > 0 {
-		// if !isPathLoop(path, len(mz)) {
-		// 	// Domain wall does not form a loop. Keep adding zero crossings to it until we run out or until it forms a
-		// 	// loop. Pop the zc nearest to the current zc.
-		// 	zcs, buffer = popMin(zcs, path[len(path)-1], len(mz))
-		// 	path = append(path, buffer)
-		// } else if pathSpansY(path, mz) {
-		// 	// The domain wall forms a loop, and all the y-indices in the simulation are covered. There are leftover
-		// 	// zcs, so there must be a bubble somewhere. Ignore.
-		// 	return path
-		// } else {
-		// 	// The domain wall forms a loop, but does not span every y-index in the simulation. Therefore a bubble must
-		// 	// have been traced, not the actual domain wall. Throw out all the ZCs in the current path
-		// 	// (which form the bubble), and start tracing a new path using a different ZC from the remaining zcs.
-		// 	zcs, buffer = pop(zcs)
-		// 	path = make([][2]int, 1)
-		// 	path[0] = buffer
-		// }
 		if pathSpansY(path, mz) {
 			return path
-		} else if isPathLoop(zcs, path, len(mz)) {
-			zcs, buffer = pop(zcs)
-			path = make([][2]int, 1)
-			path[0] = buffer
-		} else {
-			zcs, buffer = popMin(zcs, path[len(path)-1], len(mz))
-			path = append(path, buffer)
 		}
+		zcs, buffer = popMin(zcs, path[len(path)-1], len(mz))
+
+		// Append is fine here because path only ever expands.
+		path = append(path, buffer)
 	}
 	return path
 }
 
 func pop(a [][2]int) ([][2]int, [2]int) {
-	ret := a[len(a)-1]
-	a = a[:len(a)-1]
-	return a, ret
+	return a[:len(a)-1], a[len(a)-1]
 }
 
 func popMin(zcs [][2]int, b [2]int, ny int) ([][2]int, [2]int) {
@@ -71,7 +51,13 @@ func popMin(zcs [][2]int, b [2]int, ny int) ([][2]int, [2]int) {
 		}
 	}
 
-	return append(zcs[:minIndex], zcs[minIndex+1:]...), zcs[minIndex]
+	// Hideous bodge avoids unexpected behavior when using append(), which can modify the input parameters. In this
+	// case, it modifies the array underlying the input zcs slice, which is needed later on in the next iteration.
+	minZC := zcs[minIndex]
+	remainingZCs := make([][2]int, len(zcs)-1)
+	copy(remainingZCs[:minIndex], zcs[:minIndex])
+	copy(remainingZCs[minIndex:], zcs[minIndex+1:])
+	return remainingZCs, minZC
 }
 
 // Checks if path explicitly covers every y-index of the simulation region
@@ -82,23 +68,6 @@ func pathSpansY(path [][2]int, mz [][]float32) bool {
 		}
 	}
 	return true
-}
-
-// Checks whether the path forms a loop: the first point in the path is appended to the list of remaining ZCs. If the
-// closest cell to the last element in the path is the first point, then return true. Otherwise, false.
-func isPathLoop(zcs, path [][2]int, ny int) bool {
-
-	if len(path) < 3 {
-		return false
-	}
-	// _, nextZC := popMin(append(zcs, path[0]), path[len(path)-1], ny)
-
-	dists := allDistances(zcs, path[-1], ny)
-	starting_
-
-
-
-	return nextZC[0] == path[0][0] && nextZC[1] == path[0][1]
 }
 
 // Check whether a y-index iy is in the pathz
@@ -117,6 +86,8 @@ func allZCs(mz [][]float32) [][2]int {
 	for iy := range mz {
 		for ix := len(mz[iy]) - 1; ix > 1; ix-- {
 			if mz[iy][ix]*mz[iy][ix-1] < 0 {
+
+				// Append is fine here, because the array is expanding, and we have no need to access the input array
 				zcs = append(zcs, [2]int{iy, ix})
 			}
 		}
@@ -159,8 +130,8 @@ func allDistances(a [][2]int, b [2]int, ny int) []float64 {
 }
 
 func distance(a, b [2]int, ny int) float64 {
-	_dy := float64(a[0] - b[0])
-	dy := math.Min(float64(ny)-math.Abs(_dy), math.Abs(_dy)) // Wrap the distance around in the y-direction
+	dyAbs := math.Abs(float64(a[0] - b[0]))
+	dy := math.Min(float64(ny)-dyAbs, dyAbs) // Wrap the distance around in the y-direction
 	dx := float64(a[1] - b[1])
 	return math.Sqrt(dx*dx + dy*dy)
 }

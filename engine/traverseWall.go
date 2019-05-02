a8d36e476a93752e56107b0f7cf31f2bfe89cd9a
pdmurray <peyton.murray@tuni.fi>
2019-05-02 14:52:15 +0300

    Traversing the wall now scans in both x and y directions for zero crossings.

--- engine/traverseWall.go
+++ engine/traverseWall.go
@@ -21,6 +21,7 @@ func traceWall2D(mz [][]float32) [][2]int {
 	buffer := [2]int{}
 	path := make([][2]int, 1)
 
+	// zcs := xRasterZCs(mz)
 	zcs := allZCs(mz)
 	zcs, buffer = pop(zcs)
 	path[0] = buffer
@@ -37,10 +38,12 @@ func traceWall2D(mz [][]float32) [][2]int {
 	return path
 }
 
+// Remove the first element from a, returning a tuple: (remaining list, first element)
 func pop(a [][2]int) ([][2]int, [2]int) {
 	return a[:len(a)-1], a[len(a)-1]
 }
 
+// Remove the element from zcs which is the nearest to b. Returns a tuple: (remaining zcs, closest element)
 func popMin(zcs [][2]int, b [2]int, ny int) ([][2]int, [2]int) {
 	dsts := allDistances(zcs, b, ny)
 	minIndex := 0
@@ -52,7 +55,7 @@ func popMin(zcs [][2]int, b [2]int, ny int) ([][2]int, [2]int) {
 	}
 
 	// Hideous bodge avoids unexpected behavior when using append(), which can modify the input parameters. In this
-	// case, it modifies the array underlying the input zcs slice, which is needed later on in the next iteration.
+	// case, append modifies the array underlying the input zcs slice, which is needed later on in the next iteration.
 	minZC := zcs[minIndex]
 	remainingZCs := make([][2]int, len(zcs)-1)
 	copy(remainingZCs[:minIndex], zcs[:minIndex])
@@ -80,8 +83,8 @@ func iyInPath(path [][2]int, iy int) bool {
 	return false
 }
 
-// Return an array of {iy, ix} pairs of indices for each zero crossing in mz
-func allZCs(mz [][]float32) [][2]int {
+// Return an array of {iy, ix} pairs of indices for each zero crossing in mz, scanning across rows
+func xRasterZCs(mz [][]float32) [][2]int {
 	zcs := make([][2]int, 0)
 	for iy := range mz {
 		for ix := len(mz[iy]) - 1; ix > 1; ix-- {
@@ -94,6 +97,26 @@ func allZCs(mz [][]float32) [][2]int {
 	}
 	return zcs
 }
+
+// Return an array of {iy, ix} pairs of indices for each zero crossing in mz, scanning across columns
+func yRasterZCs(mz [][]float32) [][2]int {
+	zcs := make([][2]int, 0)
+	for ix := range mz[0] {
+		for iy := len(mz) - 1; iy > 1; iy-- {
+			if mz[iy][ix]*mz[iy-1][ix] < 0 {
+
+				// Append is fine here, because the array is expanding, and we have no need to access the input array
+				zcs = append(zcs, [2]int{iy, ix})
+			}
+		}
+	}
+	return zcs
+}
+
+func allZCs(mz [][]float32) [][2]int {
+	return uniqueZCs(xRasterZCs(mz), yRasterZCs(mz))
+}
+
 
 // Sort a by the cartesian distance between a[i] and b using insertion sort.
 func insertionSort(a [][2]int, b [2]int, ny int) {
@@ -141,4 +164,28 @@ func intMin(a, b int) int {
 		return a
 	}
 	return b
+}
+
+// Given two lists of tuples, return a list of all the unique tuples.
+func uniqueZCs(a, b [][2]int) [][2]int {
+	zcs := make([][2]int, len(a)) // Allocate memory for the smallest possible list size
+	for i := range a {
+		zcs[i] = a[i]
+	}
+	for i := range b {
+		if ! isIn(zcs, b[i]) {
+			zcs = append(zcs, b[i])
+		}
+	}
+	return zcs
+}
+
+// Return true if tuple b is in list of tuples a; otherwise false.
+func isIn(a [][2]int, b [2]int) bool {
+	for i := range a {
+		if b[0] == a[i][0] && b[1] == a[i][1] {
+			return true
+		}
+	}
+	return false
 }

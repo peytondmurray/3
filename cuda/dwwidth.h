f64869d73b99c14516761b2f4c23a55ef34a93b3
pdmurray <peynmurray@gmail.com>
2019-05-12 22:05:43 +0300

    Update header for dwwidth

--- cuda/dwwidth.h
+++ cuda/dwwidth.h
@@ -1,8 +1,8 @@
 #ifndef _DWWIDTH_H_
 #define _DWWIDTH_H_
 
-extern "C" __device__ float indexMulSumAtanh(float *, int, int, int, int, int, int, int);
-extern "C" __device__ float sumAtanh(float *, int, int, int, int, int, int, int);
+extern "C" __device__ float atanhMulIndSum(float *, int, int, int, int, int, int, int);
+extern "C" __device__ float atanhSum(float *, int, int, int, int, int, int, int);
 extern "C" __device__ int zcAlongY(float *, int, int, int, int, int);
 #endif
 
